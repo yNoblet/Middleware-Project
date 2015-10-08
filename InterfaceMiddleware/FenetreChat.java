5826ab1fdce468446b6525627db23d10dd9a1a87
TPichaud <thibaut.pichaud@gmail.com>
2015-10-08 17:22:00 +0200

    Modification interface Topic et Chat.java

--- InterfaceMiddleware/FenetreChat.java
+++ InterfaceMiddleware/FenetreChat.java
@@ -16,8 +16,10 @@ import javafx.scene.control.Button;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
@@ -25,75 +27,119 @@ import javafx.stage.Stage;
 
 
 public class FenetreChat extends Application {
-	
-    private TextField input;
-    private TextArea output; 
-    private final static String newline = "\n";
-    String Topic;
 
-    public void start(Stage primaryStage) {
- 
-    	GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+	private TextField input;
+	private TextArea output; 
+	private final static String newline = "\n";
+	String Topic;
+	String Identifiants;
 
- Scene scene = new Scene(grid, 750, 300);
+	public void start(Stage primaryStage) {
 
- primaryStage.setTitle("Forum de discussion");
- primaryStage.setScene(scene);
- primaryStage.show();
-        
-        Button btnR = new Button();
-        btnR.setText("Retour");
-        grid.add(btnR, 1,0);
-        btnR.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                    	
-                    	openFenetreTopic();
-                    
-                    }
-                    public void openFenetreTopic(){
-                    	FenetreTopic ft = new FenetreTopic();
-                		ft.start(primaryStage);
-                    }
-                });
-        
-        Text scenetitle = new Text("Topic "+ Topic + " :");
-        grid.add(scenetitle, 0,0);
-        
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
 
-        output = new TextArea();
-        grid.add(output, 0,1);
-        output.setEditable(false);
-        output.setStyle("-fx-border-style: none");
-        output.setFocusTraversable(false);
+		Scene scene = new Scene(grid, 750, 300);
 
-        input = new TextField();
-        grid.add(input,0,2);
-        
-        Button btn = new Button();
-        btn.setText("Valider");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	String text = input.getText();
-			    output.appendText(text + newline);
-			    input.selectAll();
-            }
-       
-        });
-        grid.add(btn, 1,2);
-    }
-    public void GetTopic (String Name){
-    	Topic = Name;
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
-    
+		primaryStage.setTitle("Forum de discussion");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+		Button btnDeco = new Button();
+		btnDeco.setText("Déconnexion");
+		btnDeco.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				openFenetreConnexion();
+			}
+			public void openFenetreConnexion(){
+				FenetreConnexion ft = new FenetreConnexion();
+				ft.start(primaryStage);
+				System.out.println("zozo)");
+
+			}
+		});
+
+		Button btnR = new Button();
+		btnR.setText("Retour");
+		grid.add(btnR, 1,1);
+		btnR.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+
+						openFenetreTopic();
+
+					}
+					public void openFenetreTopic(){
+						FenetreTopic ft = new FenetreTopic();
+						ft.start(primaryStage);
+					}
+				});
+
+		Text scenetitle = new Text("Topic "+ Topic + " :");
+		grid.add(scenetitle, 0,1);
+
+		Text identifiants = new Text("Bienvenue "+ Identifiants +" !");
+		
+		HBox hbButtons = new HBox();
+		hbButtons.getChildren().addAll(identifiants);
+		VBox vbButtons = new VBox();
+		vbButtons.setSpacing(5);
+		vbButtons.getChildren().addAll(btnDeco,btnR);
+		
+		
+		grid.add(hbButtons, 0,0);
+		grid.add(vbButtons, 1,0);
+
+		output = new TextArea();
+		grid.add(output, 0,2);
+		output.setEditable(false);
+		output.setStyle("-fx-border-style: none");
+		output.setFocusTraversable(false);
+
+		input = new TextField();
+		input.setOnKeyPressed(new EventHandler<KeyEvent>() {
+			@Override
+			public void handle(KeyEvent event) {
+				if (event.getCode() == KeyCode.ENTER) {
+					AfficherMsg();
+				}
+
+			}
+		});
+		grid.add(input,0,3);
+
+		Button btn = new Button();
+		btn.setText("Valider");
+		btn.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				AfficherMsg();
+			}
+
+		});
+		grid.add(btn, 1,3);
+	}
+
+
+	public void GetTopic (String Top, String Name){
+		Topic = Top;
+		Identifiants= Name;
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+	public  void AfficherMsg(){
+		String text = input.getText();
+		output.appendText(Identifiants +" : "+text + newline);
+		input.selectAll();
+	}
+
+
 }
--- InterfaceMiddleware/FenetreTopic.java
+++ InterfaceMiddleware/FenetreTopic.java
@@ -12,6 +12,7 @@ import javafx.scene.control.ListView;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
 import javafx.stage.Modality;
@@ -19,180 +20,175 @@ import javafx.stage.Stage;
 
 
 public class FenetreTopic extends Application {
-	
-    private TextField input;
-    private TextArea output; 
-    private final static String newline = "\n";
-    String Identifiants;
 
-    public void start(Stage primaryStage) {
- 
-    	GridPane grid = new GridPane();
-        grid.setAlignment(Pos.CENTER);
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(25, 25, 25, 25));
+	private TextField input;
+	private TextArea output; 
+	private final static String newline = "\n";
+	String Identifiants;
 
- Scene scene = new Scene(grid, 500, 300);
+	public void start(Stage primaryStage) {
 
- primaryStage.setTitle("Forum de discussion");
- primaryStage.setScene(scene);
- primaryStage.show();
-        
+		GridPane grid = new GridPane();
+		grid.setAlignment(Pos.CENTER);
+		grid.setHgap(10);
+		grid.setVgap(10);
+		grid.setPadding(new Insets(25, 25, 25, 25));
 
-        
-        Text identifiants = new Text("Bienvenue "+ Identifiants +" !");
-        
-        
-        Text TopicsIns = new Text("Topics inscrits :");
-        
-        
-        Button btnNew = new Button();
-        btnNew.setText("Nouveau");
-        btnNew.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                        final Stage dialog = new Stage();
-                        dialog.initModality(Modality.APPLICATION_MODAL);
-                        dialog.initOwner(primaryStage);
-                        VBox dialogVbox = new VBox(20);
-                        dialogVbox.getChildren().add(new Text("Choisissez le titre du nouveau Topic :"));
-                        TextField Topic = new TextField();
-                        dialogVbox.getChildren().add(Topic);
-                        Button btnNT = new Button();
-                        dialogVbox.getChildren().add(btnNT);
-                        btnNT.setText("Créer");
-                        Scene dialogScene = new Scene(dialogVbox, 300, 120);
-                        dialog.setScene(dialogScene);
-                       
-                        dialog.show();
-                        btnNT.setOnAction(
-                                new EventHandler<ActionEvent>() {
-                                    @Override
-                                    public void handle(ActionEvent event) {
-                                    	
-                                    	openFenetreChat();
-                                    	dialog.close();
-                                    }
-                                    public void openFenetreChat(){
-                                		FenetreChat ft = new FenetreChat();
-                                		ft.start(primaryStage);
-                                		System.out.println("zozo)");
-                                		
-                                	}
-                                });
-                    }
-                 });
-        
-        
-        Button btnDeco = new Button();
-        btnDeco.setText("Déconnexion");
-        btnDeco.setOnAction(new EventHandler<ActionEvent>() {
- 
-            @Override
-            public void handle(ActionEvent event) {
-            	openFenetreConnexion();
-            }
-            public void openFenetreConnexion(){
-        		FenetreConnexion ft = new FenetreConnexion();
-        		ft.start(primaryStage);
-        		System.out.println("zozo)");
-        		
-        	}
-        });
-       
-        
-        ListView<String> ListInscrits = new ListView<String>();
-        ObservableList<String> items =FXCollections.observableArrayList (
-            "Soins et Beauté", "API Rest", "Chaîne de Markov");
-        ListInscrits.setItems(items);
-        
-        
-        
-        Button btnGo = new Button();
-        btnGo.setText("Aller");
-       
-        btnGo.setOnAction(
-                new EventHandler<ActionEvent>() {
-                    @Override
-                    public void handle(ActionEvent event) {
-                    	
-                    	openFenetreChat();
-                    	
-                    }
-                    public void openFenetreChat(){
-                		FenetreChat ft = new FenetreChat();
-                		ft.start(primaryStage);
-                		System.out.println("zozo)");
-                		
-                	}
-                });
-        
-        
-        
-        Text TopicsDispos = new Text("Topics disponibles :");
-       
-        
-        
-        ListView<String> ListDispo = new ListView<String>();
-        ObservableList<String> items2 =FXCollections.observableArrayList (
-            "Toilettage canin", "toto", "zob", "zaza","GL Squad", "Robespierre", "Titeuf");
-        ListDispo.setItems(items2);
-        
-        
-        Button btnInscri = new Button();
-        btnInscri.setText("S'inscrire");
-        
-        btnInscri.setOnAction(
-        new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                final Stage dialog = new Stage();
-                dialog.initModality(Modality.APPLICATION_MODAL);
-                dialog.initOwner(primaryStage);
-                VBox dialogVbox = new VBox(20);
-                dialogVbox.getChildren().add(new Text("Topic créé !"));
-                Button btnNT = new Button();
-                dialogVbox.getChildren().add(btnNT);
-                btnNT.setText("Rejoindre");
-                Scene dialogScene = new Scene(dialogVbox, 90, 70);
-                dialog.setScene(dialogScene);
-               
-                dialog.show();
-                btnNT.setOnAction(
-                        new EventHandler<ActionEvent>() {
-                            @Override
-                            public void handle(ActionEvent event) {
-                            	
-                            	openFenetreChat();
-                            	dialog.close();
-                            }
-                            public void openFenetreChat(){
-                        		FenetreChat ft = new FenetreChat();
-                        		ft.start(primaryStage);
-                        		System.out.println("zozo)");
-                        		
-                        	}
-                        });
-            }
-        });
-        
-        grid.add(identifiants, 0,0);
-        grid.add(btnNew, 1,0);
-        grid.add(TopicsIns, 0,1);
-        grid.add(btnDeco, 1,1);
-        grid.add(ListInscrits, 0,2);
-        grid.add(btnGo, 0,3);
-        grid.add(TopicsDispos, 0,4);
-        grid.add(ListDispo, 0,5);
-        grid.add(btnInscri, 0,6);
-        
-    }
-    public void getID (String ID) {
-    	Identifiants = ID;
-    }
-    public static void main(String[] args) {
-        launch(args);
-    }
+		Scene scene = new Scene(grid, 500, 500);
+
+		primaryStage.setTitle("Forum de discussion");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+
+
+
+		Text identifiants = new Text("Bienvenue "+ Identifiants);
+
+
+		Text TopicsIns = new Text("Topics inscrits :");
+
+
+		Button btnNew = new Button();
+		btnNew.setText("Nouveau");
+		btnNew.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						final Stage dialog = new Stage();
+						dialog.initModality(Modality.APPLICATION_MODAL);
+						dialog.initOwner(primaryStage);
+						VBox dialogVbox = new VBox(20);
+						dialogVbox.getChildren().add(new Text("Choisissez le titre du nouveau Topic :"));
+						TextField Topic = new TextField();
+						dialogVbox.getChildren().add(Topic);
+						Button btnNT = new Button();
+						dialogVbox.getChildren().add(btnNT);
+						btnNT.setText("Créer");
+						Scene dialogScene = new Scene(dialogVbox, 300, 120);
+						dialog.setScene(dialogScene);
+
+						dialog.show();
+						btnNT.setOnAction(
+								new EventHandler<ActionEvent>() {
+									@Override
+									public void handle(ActionEvent event) {
+
+										openFenetreChat();
+										dialog.close();
+									}
+									public void openFenetreChat(){
+										FenetreChat ft = new FenetreChat();
+										ft.GetTopic(Topic.getText(),Identifiants);
+										ft.start(primaryStage);
+										System.out.println("zozo)");
+
+									}
+								});
+					}
+				});
+
+
+		Button btnDeco = new Button();
+		btnDeco.setText("Déconnexion");
+		btnDeco.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent event) {
+				openFenetreConnexion();
+			}
+			public void openFenetreConnexion(){
+				FenetreConnexion ft = new FenetreConnexion();
+				ft.start(primaryStage);
+				System.out.println("zozo)");
+
+			}
+		});
+
+
+		ListView<String> ListInscrits = new ListView<String>();
+		ObservableList<String> items =FXCollections.observableArrayList (
+				"Soins et Beauté", "API Rest", "Chaîne de Markov");
+		ListInscrits.setItems(items);
+
+		
+
+		Button btnGo = new Button();
+		btnGo.setText("Aller");
+
+		btnGo.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+
+						openFenetreChat();
+
+
+					}
+					public void openFenetreChat(){
+						FenetreChat ft = new FenetreChat();
+						ft.GetTopic(ListInscrits.getSelectionModel().getSelectedItem(), Identifiants);
+						ft.start(primaryStage);
+						System.out.println("zozo)");
+
+					}
+				});
+		
+		Button btnDes = new Button();
+		btnDes.setText("Se désinscrire");
+		btnDes.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+
+						
+
+
+					}
+				});
+
+
+		Text TopicsDispos = new Text("Topics disponibles :");
+
+
+
+		ListView<String> ListDispo = new ListView<String>();
+		ObservableList<String> items2 =FXCollections.observableArrayList (
+				"Toilettage canin", "toto", "zob", "zaza","GL Squad", "Robespierre", "Titeuf");
+		ListDispo.setItems(items2);
+
+
+		Button btnInscri = new Button();
+		btnInscri.setText("S'inscrire");
+
+		btnInscri.setOnAction(
+				new EventHandler<ActionEvent>() {
+					@Override
+					public void handle(ActionEvent event) {
+						
+					}
+				});
+		HBox vbButtons = new HBox();
+		vbButtons.setSpacing(150);
+		vbButtons.getChildren().addAll(identifiants,btnDeco);
+		HBox InsButtons = new HBox();
+		InsButtons.setSpacing(10);
+		InsButtons.getChildren().addAll(btnGo,btnDes);
+
+
+		grid.add(vbButtons, 0,0);
+		//        grid.add(btnNew      , 1,0);
+		        grid.add(TopicsIns  , 0,1);
+		       grid.add(ListInscrits, 0,2);
+		       grid.add(InsButtons   , 0,3);
+		       grid.add(TopicsDispos, 0,4);
+	           grid.add(ListDispo   , 0,5);
+		       grid.add(btnInscri    , 0,6);
+
+	}
+	public void getID (String ID) {
+		Identifiants = ID;
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
